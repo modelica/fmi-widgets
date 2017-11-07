fb36788974d627b1c20bd20f4c075100cec734d1
Michael Tiller <michael.tiller@gmail.com>
2017-11-07 08:14:04 -0500

    Added graph, but it doesn't look so good.

---
+++ src/components/support_matrix/support_graph.tsx
@@ -0,0 +1,58 @@
+import { MatrixReport } from 'fmi-database';
+import { observer } from 'mobx-react';
+import * as React from 'react';
+
+var Graph = require('react-graph-vis').default;
+
+type NodeType = { id: string | number, label: string, title?: string };
+type EdgeType = { from: string | number, to: string | number };
+
+@observer
+export class SupportGraph extends React.Component<{ matrix: MatrixReport }, {}> {
+    graph: { nodes: NodeType[], edges: EdgeType[] } = {
+        nodes: [
+        ],
+        edges: [
+        ]
+    };
+    options = {
+        layout: {
+            hierarchical: false
+        },
+        edges: {
+            color: "#000000"
+        },
+        interaction: {
+            hideEdgesOnDrag: true,
+            tooltipDelay: 200
+        },
+    };
+
+    constructor(props?: { matrix: MatrixReport }, context?: {}) {
+        super(props, context);
+        this.props.matrix.exporters.forEach((row, ri) => {
+            this.graph.nodes.push({
+                id: row.id,
+                label: ri.toFixed(0),
+                title: row.name,
+            });
+            row.columns.forEach((col) => {
+                this.graph.edges.push({
+                    from: row.id,
+                    to: col.id,
+                });
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={{ width: "500px", height: "500px", border: "1px solid blue" }}>
+                    <Graph graph={this.graph} options={this.options} />
+                </div>
+            </div>
+        );
+    }
+}
+
