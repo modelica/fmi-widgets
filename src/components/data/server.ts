26265b3f970cb8e2e63155d20808662ade9c58e0
Michael Tiller <michael.tiller@gmail.com>
2018-02-02 18:20:31 -0500

    Made a note about old API querying code.

--- src/components/data/server.ts
+++ src/components/data/server.ts
@@ -1,21 +1,29 @@
-import { MatrixReport } from '@modelica/fmi-data';
+import { MatrixReport } from "@modelica/fmi-data";
 
 const versionKey = "version";
 const variantKey = "variant";
 const platformKey = "platform";
 
-export async function queryServer(version: string | undefined, variant: string | undefined, platform: string | undefined): Promise<MatrixReport> {
+// NB - This code is old and needs to be upgraded to return a QueryResult instead of a MatrixReport (if
+// we ever decide to use it)
+export async function queryServer(
+    version: string | undefined,
+    variant: string | undefined,
+    platform: string | undefined,
+): Promise<MatrixReport> {
     let qs: { [key: string]: string } = {};
     if (version) qs[versionKey] = version;
     if (variant) qs[variantKey] = variant;
     if (platform) qs[platformKey] = platform;
 
     let headers = new Headers({
-        "Accept": "application/json",
+        Accept: "application/json",
     });
     headers.set("Accept", "application/json");
 
-    let qsstr = Object.keys(qs).map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(qs[k])).join("&");
+    let qsstr = Object.keys(qs)
+        .map(k => encodeURIComponent(k) + "=" + encodeURIComponent(qs[k]))
+        .join("&");
 
     let req = new Request(`http://localhost:4000/report/matrix?${qsstr}`, {
         method: "GET",
